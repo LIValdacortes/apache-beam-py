a963d75eda925c14f1417d3126ae8f2b89fefb4e
AldaCortesLiv <jacortesl@liverpool.com.mx>
2022-08-03 19:07:18 -0500

    WIP

--- main.py
+++ main.py
@@ -11,3 +11,37 @@
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
+import apache_beam as beam
+import argparse
+from ast import parse
+
+from apache_beam.options_pipeline_options import PipelineOptios
+
+def main():
+    #leer argumentos de entrada cli
+    parser = argparse.ArgumentParser(description="Primer Pipeline")
+    parser.add_argument("--entrada", help="Fichero de entrada")
+    parser.add_argument("--salida", help="Fichero de salida")
+
+    our_args, beam_args = parser.parse_known_args()
+    run_pipeline(our_args, beam_args)
+
+def run_pipeline(custom_args,beam_args):
+    entrada = custom_args.entrada
+    salida = custom_args.salida
+
+    opts = PipelineOptios(beam_args)
+
+    with beam.Pipeline(opts) as p:
+        lineas: PCollection[str] = p | beam.io.ReadFromText(entrada)
+        palabras = lineas | beam.FlatMap(lambda  l : l.split())
+        contadas: PCollection[Tuple[str,int]] = palabras | beam.combiners.Count.PerElement()
+        palabras_top = contadas | beam.combiners.Top.Of(5, keys = lambda kv: kv[1])
+
+        palabras_top | beam.Map(print)
+
+
+
+
+if __name__ == '__main__':
+    main()
